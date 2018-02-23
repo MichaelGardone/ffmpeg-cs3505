ab3e0852a06dc2e2488b9b6204ad32a14d9853c8
takuuuu517 <ryoryoryoasa@gmail.com>
2018-02-23 16:10:09 -0700

    revert

--- CP2/nicedec.c
+++ CP2/nicedec.c
@@ -1,5 +1,6 @@
 /*
- * NICE is a file format created for CS3505 Spring 2018 class.
+ * BMP image format decoder
+ * Copyright (c) 2005 Mans Rullgard
  *
  * This file is part of FFmpeg.
  *
@@ -18,157 +19,124 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavutil/imgutils.h"
-#include "libavutil/avassert.h"
+#include <inttypes.h>
+
 #include "avcodec.h"
 #include "bytestream.h"
 #include "nice.h"
 #include "internal.h"
+#include "msrledec.h"
 
-static ColorTable ct[256];
-
-
-
-/**
- * Initialize the color table and set the pix format to BGR 24
- **/
-static av_cold int nice_encode_init(AVCodecContext *avctx){
-    populate_ColorTable(&ct);
-    
-    switch (avctx->pix_fmt) {
-        case AV_PIX_FMT_BGR24:
-            avctx->bits_per_coded_sample = 24;
-            break;
-        default:
-            av_log(avctx, AV_LOG_INFO, "unsupported pixel format\n");
-            return AVERROR(EINVAL);
-    }
-    return 0;
-}
-
-/**
- * Nice encode frame encodes the image file in a nice format. NICE format contains header: "NICE", width, and height.
- **/
-static int nice_encode_frame(AVCodecContext *avctx, AVPacket *pkt, const AVFrame *pict, int *got_packet)
+static int nice_decode_frame(AVCodecContext *avctx,
+                             void *data, int *got_frame,
+                             AVPacket *avpkt)
 {
-    const AVFrame * const p = pict;
-    // hsize - header size
-    // n_bytes - size of file/number of bytes in file
-    // n_bytes_image - size of pixel array
-    // i - for loop
-    // n - second for loop, internal for loop
-    // ret - return value for error
-    int n_bytes_image, n_bytes_per_row, n_bytes, i, n, hsize, ret;
-    const uint32_t *pal = NULL;
-    // pad_bytes_per_row - padding between rows
-    // pal_entires - ?????
-    int pad_bytes_per_row=0, pal_entries = 0;
-    // bit count - color channel
-    int bit_count = avctx->bits_per_coded_sample;
-    // *buf - buffer pointer
-    // *ptr - points to the end of the file
-    uint8_t *ptr, *end_ptr, *buf;
+    ColorTable ct[256];
+    populate_ColorTable(&ct);
+    const uint8_t *buf = avpkt->data;
+    int buf_size       = avpkt->size;
+    AVFrame *p         = data;
+    int width, height;
+    unsigned int depth;
+    unsigned int ihsize;
+    int i, j, n, linesize, ret;
+    uint32_t rgb[3] = {0};
+    uint32_t alpha = 0;
+    uint8_t *ptr, *t_ptr;
+    int dsize;
     
-    // GUARD AGAINST DEPRECATED CODE
-#if FF_API_CODED_FRAME
-    FF_DISABLE_DEPRECATION_WARNINGS
-    avctx->coded_frame->pict_type = AV_PICTURE_TYPE_I;
-    avctx->coded_frame->key_frame = 1;
-    FF_ENABLE_DEPRECATION_WARNINGS
-#endif
-    // GUARD AGAINST DEPRECATED CODE
+    int n_bytes_image;
+    const uint8_t *buf0 = buf;
+    GetByteContext gb;
     
-    if (pal && !pal_entries)pal_entries = 1 << bit_count;
+    if (buf_size < 14) {
+        av_log(avctx, AV_LOG_ERROR, "buf size too small (%d)\n", buf_size);
+        return AVERROR_INVALIDDATA;
+    }
     
-    n_bytes_per_row = ((int64_t)avctx->width * (int64_t)bit_count + 7LL ) >> 3LL;
+    if (bytestream_get_byte(&buf) != 'N' ||
+        bytestream_get_byte(&buf) != 'I' ||
+        bytestream_get_byte(&buf) != 'C' ||
+        bytestream_get_byte(&buf) != 'E') {
+        av_log(avctx, AV_LOG_ERROR, "bad magic number\n");
+        return AVERROR_INVALIDDATA;
+    }
     
-    n_bytes_image = avctx->height * (n_bytes_per_row + pad_bytes_per_row);
-    n_bytes_image /=3;
-    n_bytes_image += 2*avctx->height;
+    // Get width, height, and number of bytes in the image
+    width = bytestream_get_le32(&buf);
+    height = bytestream_get_le32(&buf);
     
-    // Size of the NICE Header and NICE Info Header(IH)
-#define SIZE_BITMAPFILEHEADER 4
-#define SIZE_BITMAPINFOHEADER 8
-    hsize = SIZE_BITMAPFILEHEADER + SIZE_BITMAPINFOHEADER + (pal_entries << 2);
-    n_bytes = n_bytes_image + hsize;
+    n_bytes_image = bytestream_get_le32(&buf);
     
-    // Checking to see if memory can be alloacted to a packet for putting in file
-    if ((ret = ff_alloc_packet2(avctx, pkt, n_bytes, 0)) < 0)
+    depth = 8;
+    
+    ret = ff_set_dimensions(avctx, width, height > 0 ? height : -(unsigned)height);
+    if (ret < 0) {
+        av_log(avctx, AV_LOG_ERROR, "Failed to set dimensions %d %d\n", width, height);
+        return AVERROR_INVALIDDATA;
+    }
+    
+    avctx->pix_fmt = AV_PIX_FMT_BGR24;
+    
+    if ((ret = ff_get_buffer(avctx, p, 0)) < 0)
         return ret;
     
-    buf = pkt->data;
+    p->pict_type = AV_PICTURE_TYPE_I;
+    p->key_frame = 1;
     
-    // == INSERT NICE HEADER == //
-    bytestream_put_byte(&buf, 'N');
-    bytestream_put_byte(&buf, 'I');
-    bytestream_put_byte(&buf, 'C');
-    bytestream_put_byte(&buf, 'E');
-    // == INSERT NICE HEADER == //
+    dsize = buf_size;
     
-    // == INSERT WIDTH AND HEIGHT == //
-    bytestream_put_le32(&buf, avctx->width);
-    bytestream_put_le32(&buf, avctx->height);
-    // == INSERT WIDTH AND HEIGHT == //
+    /* Line size in file multiple of 4*/
+    n = ((avctx->width * depth + 31) / 8) & ~3;
     
-    
-    // guard
-    for (i = 0; i < pal_entries; i++)
-        bytestream_put_le32(&buf, pal[i] & 0xFFFFFF);
-    
-    end_ptr = p->data[0] + (avctx->height - 1) * p->linesize[0];
-    ptr = p->data[0];
-    
-    // All encoding/compression goes here
-    // Loop through height
-    
-    
-    for(i = 0; i < avctx->height; i++) {
-        int colors[3]; // color[0] = blue, color[1] = green, color[2] = red
-        for (n = 0; n < avctx->width*3; n++) {
-            colors[n%3] = (ptr[n]);
-            if(n%3 == 2) {
-                int p;
-                
-                int colorIndex; // index nunmber that the closest color is stroed in the color table.
-                int smallestSoFar = 1000; // initialize it to 1000 because it is never bigger than 1000
-                for(p=0; p < 255; p++) {
-                    int rDiff = abs(ct[p].r - colors[2]);
-                    int gDiff = abs(ct[p].g - colors[1]);
-                    int bDiff = abs(ct[p].b - colors[0]);
-                    int sum = rDiff+gDiff+bDiff;
-                    
-                    // choose the closest color from the color table
-                    if(smallestSoFar>sum)
-                    {
-                        smallestSoFar = sum;
-                        colorIndex = p;
-                    }
-                }
-                
-                // put the index nunmber to the buffer (1 byte each)
-                bytestream_put_byte(&buf, colorIndex);
-            }
+    if (n * avctx->height > dsize) {
+        n = (avctx->width * depth + 7) / 8;
+        if (n * avctx->height > dsize) {
+            av_log(avctx, AV_LOG_ERROR, "not enough data (%d < %d)\n",
+                   dsize, n * avctx->height);
+            return AVERROR_INVALIDDATA;
         }
-        
-        
-        if(end_ptr != ptr)
-            ptr += p->linesize[0]; // increment the pointer but make sure not to point outside
+        av_log(avctx, AV_LOG_ERROR, "data size too small, assuming missing line alignment\n");
     }
     
-    pkt->flags |= AV_PKT_FLAG_KEY;
-    *got_packet = 1;
-    return 0;
+    if (height > 0) {
+        ptr      = p->data[0] + (avctx->height - 1) * p->linesize[0];
+        linesize = p->linesize[0];
+    } else {
+        ptr      = p->data[0];
+        linesize = p->linesize[0];
+    }
+    
+    // Translation here
+    int y;
+    for (y = 0; y < avctx->height; y++) {
+        int x;
+        for(x = 0; x < avctx->width; x++) {
+            uint8_t t = bytestream_get_byte(&buf);
+            int bgr[3];
+            bgr[2] = ct[t].r;
+            bgr[1] = ct[t].g;
+            bgr[0] = ct[t].b;
+            
+            int rgb_cnt;
+            for(rgb_cnt = 0; rgb_cnt < 3; rgb_cnt++) {
+                p->data[0][y*p->linesize[0]+x*3+rgb_cnt] = bgr[rgb_cnt];
+            }
+        }
+        //buf += 2;
+    }
+    // Translation here
+    
+    *got_frame = 1;
+    
+    return buf_size;
 }
 
-AVCodec ff_nice_encoder = {
+AVCodec ff_nice_decoder = {
     .name           = "nice",
-    .long_name      = NULL_IF_CONFIG_SMALL("BMP (Windows and OS/2 bitmap)"),
+    .long_name      = NULL_IF_CONFIG_SMALL("Nice"),
     .type           = AVMEDIA_TYPE_VIDEO,
     .id             = AV_CODEC_ID_NICE,
-    .init           = nice_encode_init,
-    .encode2        = nice_encode_frame,
-    .pix_fmts       = (const enum AVPixelFormat[]){
-        AV_PIX_FMT_BGR24,
-        AV_PIX_FMT_NONE
-    },
+    .decode         = nice_decode_frame,
+    .capabilities   = AV_CODEC_CAP_DR1,
 };
